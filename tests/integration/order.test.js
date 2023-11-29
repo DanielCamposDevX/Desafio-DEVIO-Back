02c50729d19ec1954568b8f996f9a62e43a80d84
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-11-29 20:07:17 -0300

    feat/ Tests Route POST Order Coverage >90%

--- tests/integration/order.test.js
+++ tests/integration/order.test.js
@@ -1,8 +1,10 @@
 import supertest from "supertest"
 import app from "../../src/app"
 import { CleanDB } from "../helpers"
-import { createOrder } from "../factories/order.factory"
+import { createOrder, createUser } from "../factories/order.factory"
 import { faker } from "@faker-js/faker"
+import { createExtra } from "../factories/extra.factories"
+import { createFood } from "../factories/food.factories"
 
 
 const server = supertest(app)
@@ -31,7 +33,7 @@ describe("Food Tests", () => {
 
     describe("Route DELETE /orders", () => {
         it("Should respond with 404 when giving a unexistent id", async () => {
-            const response = await server.delete(`/orders/${faker.number.int({ min: 1000,max: 10000 })}`);
+            const response = await server.delete(`/orders/${faker.number.int({ min: 1000, max: 10000 })}`);
             expect(response.status).toBe(404);
         });
         it("Should respond with 400 when giving a invalid id", async () => {
@@ -43,6 +45,98 @@ describe("Food Tests", () => {
             const response = await server.delete(`/orders/${order.id}`);
             expect(response.status).toBe(200);
         })
+    })
+    describe("Route POST /orders", () => {
 
+        it("Should respond with 422 when sending a invalid body", async () => {
+            const response = await server.post("/orders").send({ name: faker.lorem.word() });
+            expect(response.status).toBe(422);
+        })
+
+        it("Should respond with 400 when user dont exist", async () => {
+            const extra1 = await createExtra();
+            const extra2 = await createExtra()
+            const food1 = await createFood();
+            const food2 = await createFood();
+
+
+            const body = {
+                userId: faker.number.int({min:1000,max:10000}),
+                observation: faker.lorem.paragraph(),
+                items: [
+                    {
+                        extras: [extra1.id, extra2.id],
+                        quantity: 1,
+                        foodId: food1.id,
+                    },
+                    {
+                        extras: [extra1.id, extra2.id],
+                        quantity: 1,
+                        foodId: food2.id,
+                    }
+                ]
+            };
+            const response = await server.post("/orders").send(body);
+            expect(response.status).toBe(400);
+        })
+
+        it("Should respond with 404 when sending a not existing food or extra", async () => {
+            const user = await createUser();
+            const food1 = await createFood();
+            const food2 = await createFood();
+
+
+            const body = {
+                userId: user.id,
+                observation: faker.lorem.paragraph(),
+                items: [
+                    {
+                        extras: [faker.number.int({min:1000,max:10000})],
+                        quantity: 1,
+                        foodId: food1.id,
+                    },
+                    {
+                        extras: [faker.number.int({min:1000,max:10000})],
+                        quantity: 1,
+                        foodId: food2.id,
+                    }
+                ]
+            };
+            const response = await server.post("/orders").send(body);
+            expect(response.status).toBe(404);
+        })
+
+
+
+
+
+
+        it("Should create an order", async () => {
+            const user = await createUser();
+            const extra1 = await createExtra();
+            const extra2 = await createExtra()
+            const food1 = await createFood();
+            const food2 = await createFood();
+
+
+            const body = {
+                userId: user.id,
+                observation: faker.lorem.paragraph(),
+                items: [
+                    {
+                        extras: [extra1.id, extra2.id],
+                        quantity: 1,
+                        foodId: food1.id,
+                    },
+                    {
+                        extras: [extra1.id, extra2.id],
+                        quantity: 1,
+                        foodId: food2.id,
+                    }
+                ]
+            };
+            const response = await server.post("/orders").send(body);
+            expect(response.status).toBe(201);
+        })
     })
 })
